2486832d6d884950f328f8d8cf2c28153faa9d76
Shivam Kumar Soni <shivam.soni.aiml@gmail.com>
2026-06-19 21:22:58 +0530

    Add files via upload

---
+++ 1_Program.py
@@ -0,0 +1,14 @@
+# 31/07/2025 First Program 
+
+Name="Shivam Kumar Soni"
+Age= 20
+Address="Hazaribag,Jharkhand"
+College= "Marwadi University"
+Course="B.Tech CSE AI ML"
+print(Name)
+print(Age)
+print(Address)
+print(College)
+print(Course)
+
+
