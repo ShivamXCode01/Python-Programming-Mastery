36b26b2e9f4f2c702ed1a820641e2dd77455b4ea
Shivam Kumar Soni <shivam.soni.aiml@gmail.com>
2026-06-21 20:29:00 +0530

    Add files via upload

---
+++ OOP/Class_1.py
@@ -0,0 +1,7 @@
+class Car:
+    def __init__(self,Brand,Model):
+        self.Brand=Brand
+        self.Model =Model
+
+Car_1 = Car('TATA','NEXON')
+#print(Car_1)
---
+++ OOP/Class_Instance.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+student_1 = Student('Shivam',21)
+print(student_1.name ,student_1.age)
