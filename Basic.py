0f43e9148a8027690a0e6ef17487a46fd833d040
Shivam Kumar Soni <shivam.soni.aiml@gmail.com>
2026-06-23 11:12:07 +0530

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,15 @@
+# Use to print anything we want
+print("Jai Shree Ram.")
+
+#types to datatypes
+string='Har Har Mahaddev'
+integer=108
+float_1 = 89.89
+character='e'
+complex_number=4+6j
+bool_type=True
+# used to print datatypes
+print(type(string))
+
+
+
