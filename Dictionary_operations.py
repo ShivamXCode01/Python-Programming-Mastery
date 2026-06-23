5b47b0133a2f66366aece2f35bdfee5c8f5e847e
Shivam Kumar Soni <shivam.soni.aiml@gmail.com>
2026-06-23 11:12:23 +0530

    Add files via upload

---
+++ Dictionary_operations.py
@@ -0,0 +1,11 @@
+Student ={
+            ' First name':'Shivam',
+            'Second name':'Kumar',
+             'Last name' :'Soni'
+}
+
+Student['Age']=78
+
+Student['Second name'] ='Ram' 
+print(Student)
+print(Student['Age'])
